b0a569ae8ebb7d994878ce5e88ccc5d40ea40608
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-22 16:18:35 +0000

    Added feature tests for multiplayer, which I accidentally already implemented

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -13,3 +13,23 @@ feature 'reducing points after attack' do
     expect(page).to have_content("Kim HP: 50/60")
   end
 end
+
+feature 'multiplayer updating points after 2nd attack' do
+  scenario 'after receiving an attack, Player 2 HP are reduced by 10' do
+    sign_in_and_play
+    click_button "Attack"
+    expect(page).to have_content("Kim HP: 50/60")
+    click_button "Attack"
+    expect(page).to have_content("Sam HP: 50/60")
+  end
+end
+
+feature 'message changes after 2nd attack' do
+  scenario 'after receiving an attack, Player 2 HP are reduced by 10' do
+    sign_in_and_play
+    click_button "Attack"
+    expect(page).to have_content("Sam attacked!")
+    click_button "Attack"
+    expect(page).to have_content("Kim attacked!")
+  end
+end
